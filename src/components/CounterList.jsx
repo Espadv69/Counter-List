03760d3674f9d4dd60e4f349f4a5cdc9ae0f5739
espadv69 <aitorjob69@gmail.com>
2025-01-23 11:53:11 +0100

    Add functionality to add and reset counters in CounterList component

--- src/components/CounterList.jsx
+++ src/components/CounterList.jsx
@@ -1,7 +1,20 @@
-import { useState } from "react";
+import { useState } from 'react'
 import Counter from './Counter'
 
 const CounterList = () => {
   const [counters, setCounters] = useState([])
   const [nextId, setNextId] = useState(1)
+
+  const addCounter = () => {
+    setCounters([...counters, { id: nextId, value: 0 }])
+    setNextId((nextId) => nextId + 1)
+  }
+
+  const resetCounter = (id) => {
+    setCounters(
+      counters.map((counter) =>
+        counter.id === id ? { ...counter, value: 0 } : counter
+      )
+    )
+  }
 }
