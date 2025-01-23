6ee39602a5ec28af189307163a1349a7ba4ef884
espadv69 <aitorjob69@gmail.com>
2025-01-23 11:49:52 +0100

    Add CounterList component to manage multiple Counter instances

---
+++ src/components/CounterList.jsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+import Counter from './Counter'
+
+const CounterList = () => {
+  const [counters, setCounters] = useState([])
+  const [nextId, setNextId] = useState(1)
+}
